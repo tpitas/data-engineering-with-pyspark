a9bc488d6c273cc0b7a97a09db7b597d80d51d95
Barry <tmabarry@yahoo.com>
2024-02-06 19:21:51 -0500

    Updated Schema

--- pyspark_with_schema.py
+++ pyspark_with_schema.py
@@ -9,6 +9,7 @@ schema = StructType([
    StructField('CustomerID', IntegerType(), False),
    StructField('LastName',   StringType(),  False),
    StructField('FirstName',  StringType(),  False),
+   StructField('CustomerEmail',  StringType(),  False),
    StructField('CustomerStreet',  StringType(),  False),
    StructField('CustomerCity',  StringType(),  False),
    StructField('CustomerState',   StringType(),  False),
@@ -17,11 +18,11 @@ schema = StructType([
 
 # Create a DataFrame
 data = [
-        [ 1 , 'Richard' , 'Hernandez', '6303 Heather Plaza     ', 'Brownsville', 'TX' , '78521' ], 
-        [ 2 , 'Mary   ' , 'Barrett  ', '9526 Noble Embers Ridge', 'Littleton  ', 'CO' , '80126' ], 
-        [ 3 , 'Ann    ' , 'Smith    ', '3422 Blue Pioneer Bend ', 'Caguas     ', 'PR' , '00725' ], 
-        [ 4 , 'Mary   ' , 'Jones    ', '8324 Little Common     ', 'San Marcos ', 'CA' , '92069' ], 
-        [ 5 , 'Robert ' , 'Hudson   ', '10 Crystal River Mall  ', 'Caguas     ', 'PR' , '00725' ]
+        [ 1 , 'Richard' , 'Hernandez', 'Richard.Hernandez@example.com','6303 Heather Plaza', 'Brownsville', 'TX' , '78521' ], 
+        [ 2 , 'Mary' , 'Barrett', 'Mary.Barrett@example.com','9526 Noble Embers Ridge', 'Littleton  ', 'CO' , '80126' ], 
+        [ 3 , 'Ann' , 'Smith', 'Ann.Smith@example.com','3422 Blue Pioneer Bend', 'Caguas', 'PR' , '00725' ], 
+        [ 4 , 'Mary' , 'Jones', 'Mary.Jones@example.com','8324 Little Common', 'San Marcos', 'CA' , '92069' ], 
+        [ 5 , 'Robert' , 'Hudson', 'Robert.Hudson@example.com','10 Crystal River Mall', 'Caguas', 'PR' , '00725' ]
      ]
 
 customers = spark.createDataFrame(data, schema)
